fc40b765682c8fbc801a537e498e96cbc94edfd6
javier <javier_2790@outlook.com>
2025-11-06 19:58:45 +0100

    update chat ai

--- backend/app/core/intent_detector.py
+++ backend/app/core/intent_detector.py
@@ -10,23 +10,18 @@ class Intent(str, Enum):
 
 def detect_intent(message: str) -> Intent:
     message = message.lower().strip()
-    print("estoy aqui en intent detector con el mensaje:", message)
-    # 💰 Ingresos o ganancias
+    
     if re.search(r"(gan|ingres|rent|cuánto gano|gané|cuanto gano|gane|beneficio|utilidad)", message):
         return Intent.INCOME
     
-    # 🏠 Número de propiedades
     elif re.search(r"(cuántas|cuantos|número|total).*(propiedad|propiedades)", message):
         return Intent.PROPERTY_COUNT
     
-    # 🏆 Propiedad con más ingresos
     elif re.search(r"(mejor|más rentable|mayor ganancia|más dinero)", message):
         return Intent.TOP_PROPERTY
 
-    # 💸 Gastos
     elif re.search(r"(gast|cost|pagué|deuda|pago)", message):
         return Intent.EXPENSES
 
-    # ❓ Desconocido
     else:
         return Intent.UNKNOWN
