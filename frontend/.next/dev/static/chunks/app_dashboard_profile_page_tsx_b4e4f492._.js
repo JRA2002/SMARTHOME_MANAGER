8792d580ae338962090f2f5649e5841a6cb3a5ed
javier <javier_2790@outlook.com>
2025-11-13 20:54:25 +0100

    Remove .next folder from repo

--- frontend/.next/dev/static/chunks/app_dashboard_profile_page_tsx_b4e4f492._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/_cdfc9dde._.js",
-  "static/chunks/node_modules_ed788838._.js"
-],
-    source: "dynamic"
-});
